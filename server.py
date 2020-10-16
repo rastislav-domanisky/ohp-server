1b9375a5749a1fd4fa1c69feb67409ee1c0faa4f
rastislav-domanisky <domanisky.rasto@gmail.com>
2020-10-16 12:51:57 +0200

    init

---
+++ server.py
@@ -0,0 +1,84 @@
+from flask import Flask
+from flask import request, jsonify
+import json
+
+app = Flask(__name__)
+
+key = "openhomepanel123"
+
+# Setup -------------------------------
+
+#TO-DO: init switch state to false !!!
+
+#with open('config.json') as json_file:
+#    data = json.load(json_file)
+#    print("Config loaded")
+
+# API ---------------------------------
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/get_pin', methods=['GET'])
+def get_pin():
+    if(request.headers.get("api_key")==key):
+        with open('config.json') as json_file:
+            data = json.load(json_file)
+        return jsonify(data["settings"][0]["pin"])
+    else:
+        return 'bad request!', 400
+
+
+@app.route('/change_pin', methods=['POST'])
+def change_pin():
+    if(request.headers.get("api_key")==key):
+        with open('config.json') as json_file:
+            data = json.load(json_file)
+        data["settings"][0]["pin"] = request.args["pin"]
+        with open('config.json', 'w') as outfile:
+            json.dump(data, outfile)
+        return 'OK', 200
+    else:
+        return 'bad request!', 400
+
+
+@app.route('/switch', methods=['POST'])
+def switch():
+    if(request.headers.get("api_key")==key):
+        with open('config.json') as json_file:
+            data = json.load(json_file)
+        swID = int(request.args["id"])
+        if(request.args["state"] == "true"):
+            swState = True
+        else:
+            swState = False
+        
+        data["switches"][swID]["state"] = swState
+        with open('config.json', 'w') as outfile:
+            json.dump(data, outfile)
+        return 'OK', 200
+    else:
+        return 'bad request!', 400
+
+
+@app.route('/get_switches', methods=['GET'])
+def get_switches():
+    if(request.headers.get("api_key")==key):
+        with open('config.json') as json_file:
+            data = json.load(json_file)
+        return jsonify(data["switches"])
+    else:
+        return 'bad request!', 400
+
+@app.route('/get_temp', methods=['GET'])
+def get_temp():
+    if(request.headers.get("api_key")==key):
+        return "23 °C"
+    else:
+        return 'bad request!', 400
+
+
+# Start server -----------------------------
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
